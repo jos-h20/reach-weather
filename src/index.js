e89407926797fe76aa8e29fc5af9d7858920c344
jos-h20 <josh_overly@yahoo.com>
2016-11-29 13:10:55 -0800

    install react-fetch, console log open weather api fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Fetch from 'react-fetch';
+
 import LocationInput from './components/location_input';
 
 const API_KEY = 'c691c22dbd26d013f884323a1acb58a0'; //open weather api key
@@ -11,10 +13,18 @@ const App = () => {
   return (
     <div>
       <LocationInput />
+      <Fetch url="http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c691c22dbd26d013f884323a1acb58a0">
+        <TestComponent/>
+      </Fetch>
     </div>
   );
 }
-
+class TestComponent extends React.Component{
+  render(){
+    console.log(this.props)
+    return <div/>
+  }
+}
 
 
 
