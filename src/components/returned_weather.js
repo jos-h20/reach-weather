646ae489d6d81a2144b379ba8a7cf5361c1b8d7e
jos-h20 <josh_overly@yahoo.com>
2016-11-29 22:26:57 -0800

    changed App component to class, initialize state, display returned weather property in child component

---
+++ src/components/returned_weather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ReturnedWeather = ({ weather }) => {
+  console.log(weather, "and shit");
+  if (!weather) {
+    return <div>Loading...</div>
+  }
+  return(
+
+    <div className="weather-detail col-md-8">
+      <div className="details">
+        <div>{weather.main.temp}</div>
+        <div>{weather.weather[0].description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ReturnedWeather;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Fetch from 'react-fetch';
+// import Fetch from 'react-fetch';
 
 import LocationInput from './components/location_input';
+import ReturnedWeather from './components/returned_weather';
 
 const API_KEY = 'c691c22dbd26d013f884323a1acb58a0'; //open weather api key
 
 
 
-const App = () => {
-  return (
-    <div>
-      <LocationInput />
-      <Fetch url="http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c691c22dbd26d013f884323a1acb58a0">
-        <TestComponent/>
-      </Fetch>
-    </div>
-  );
-}
-class TestComponent extends React.Component{
-  render(){
-    console.log(this.props)
-    return <div/>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { weather: null };
+    this.citySearch("Portland");
+
+  }
+
+  citySearch(city) {
+     city="Boston"
+     return fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + API_KEY)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        weather: responseJson
+      });
+      console.log(this.state.weather)
+
+
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <ReturnedWeather
+          weather={this.state.weather}
+        />
+      </div>
+    );
   }
 }
+
 
 
 
